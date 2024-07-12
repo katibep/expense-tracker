c92e3f7fadd6bf09ef75d51e741284f43885e6c0
katibep <162332356+katibep@users.noreply.github.com>
2024-07-12 13:29:57 -0700

    CHA

--- app.js
+++ app.js
@@ -1,22 +1,47 @@
-const overlay=document.querySelector(".overlay")
-const form=document.querySelector(".form")
-const addButton=document.querySelector(".button")
-const close=document.querySelectorAll(".close")
-const create=document.querySelector(".create")
-const tittle=document.querySelector(".tittle")
-const amount=document.querySelector("amount")
-console.log(close)
-addButton.addEventListener("click",function(){
-    form.style.display="block"
-    overlay.style.display="block"
-})
-close.forEach((close)=>{
-    close.addEventListener("click",function(){
-        form.style.display="none"
-        overlay.style.display="none"
-    }) 
-})
-form.addEventListener("submit",function(e){
-e.preventDefault()
-})
+const overlay = document.querySelector(".overlay");
+const form = document.querySelector(".form");
+const addButton = document.querySelector(".button");
+const closeButtons = document.querySelectorAll(".close");
+const create = document.querySelector(".create");
+const titleField = document.querySelector(".tittle");
+const income = document.querySelector("#income");
+const expense = document.querySelector("#expense");
+const amountField = document.querySelector(".amount");
+const transactions = [];
+addButton.addEventListener("click", function() {
+    form.style.display = "block";
+    overlay.style.display = "block";
+});
+closeButtons.forEach((close) => {
+    close.addEventListener("click", function() {
+        form.style.display = "none";
+        overlay.style.display = "none";
+    });
+});
+form.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    const title=titleField.value
+    const amount=amountField.value
+
+   let type= ''
+   if(income.checked ===  true){
+   type=income.value
+   }
+   if(expense.checked === true){
+    type=expense.value
+   }
+   
+//    here we are saving all our input data into one object
+   const inputData={
+    title,
+    amount:Number(amount),
+    type
+   }
+
+   transactions.push(inputData)
+  
+   localStorage.setItem("transactions" , JSON.stringify())
+
+   console.log(inputData)
+});
 
