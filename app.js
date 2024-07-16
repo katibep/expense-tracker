d5bce8ac708bcd21d7e98230e6a47885407f78e0
katibep <162332356+katibep@users.noreply.github.com>
2024-07-16 13:28:26 -0700

    change

--- app.js
+++ app.js
@@ -7,13 +7,21 @@ const titleField = document.querySelector(".tittle");
 const income = document.querySelector("#income");
 const expense = document.querySelector("#expense");
 const amountField = document.querySelector(".amount");
-const transactions = [];
+const transactionContainer=document.querySelector(".arrows");
+const bal=document.querySelector(".bal")
+const number1=document.querySelector(".number1")
+const number22=document.querySelector(".number2")
+
+// calling the transactions from localStorage
+const transactions = JSON.parse(localStorage.getItem("transactions")) || [];
+
+console.log(transactions)
 addButton.addEventListener("click", function() {
     form.style.display = "block";
     overlay.style.display = "block";
 });
 closeButtons.forEach((close) => {
-    close.addEventListener("click", function() {
+    close.addEventListener("click", function(){
         form.style.display = "none";
         overlay.style.display = "none";
     });
@@ -22,7 +30,7 @@ form.addEventListener("submit", (e)=>{
     e.preventDefault()
     const title=titleField.value
     const amount=amountField.value
-
+    
    let type= ''
    if(income.checked ===  true){
    type=income.value
@@ -30,7 +38,6 @@ form.addEventListener("submit", (e)=>{
    if(expense.checked === true){
     type=expense.value
    }
-   
 //    here we are saving all our input data into one object
    const inputData={
     title,
@@ -39,9 +46,38 @@ form.addEventListener("submit", (e)=>{
    }
 
    transactions.push(inputData)
-  
-   localStorage.setItem("transactions" , JSON.stringify())
+//   creating / setting / putting items in the local storage
+   localStorage.setItem("transactions" , JSON.stringify(transactions))
 
-   console.log(inputData)
+//    calling render/update ui function 
+   renderTransactions(transactions)
 });
-
+// updating ui / Rendering
+function renderTransactions(transactions) {
+    console.log(transactions)
+    transactionContainer.innerHTML = "";
+    if (transactions.length > 0) {
+      transactions.forEach((transaction) => {
+        const transactionHtml = `
+        <div class="arrows">
+        <div class="all-arrows">
+        <div class="down">
+            <div class="word">
+                ${transaction.type === "income"?"<i class='bx bx-up-arrow-alt'></i>":"<i class='bx bx-down-arrow-alt' ></i>" }
+                <p>${transaction.title} <br> <span>food</span></p>
+            </div>
+            <div class="namba">
+                <p>-${transaction.amount} <br> <span>Today</span></p>
+            </div>
+        </div>
+       </div>
+     </div>
+        `;
+        transactionContainer.insertAdjacentHTML("beforeend", transactionHtml);
+      });
+    } else {
+        transactionContainer.innerHTML = "No Transactions";
+    }
+  }
+  renderTransactions(transactions)
+  
